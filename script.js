79bb207e5bd4eb1532ec3e06dc1e74422d3069ed
Anurag Prasad <123822254+anuragpras@users.noreply.github.com>
2024-07-20 01:26:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+const projects = [
+    {
+        title: "Image Caption Generator",
+        link: "https://github.com/anuragpras/image-description-deep-learning",
+        description: "Image Caption Generator using Flickr dataset",
+        gif: "1.gif"
+    },
+    {
+        title: "Cafe Sales Analysis",
+        link: "https://github.com/anuragpras/cafe-sales-analysis",
+        description: "Detailed cafe sales analysis project using Power BI and MySQL",
+        gif: "2.gif"
+    },
+    {
+        title: "Forecasting Hotel Booking Cancellations",
+        link: "https://github.com/anuragpras/forecasting-hotel-booking-cancellations",
+        description: "Machine learning model to predict hotel booking cancellations",
+        gif: "11.gif"
+    },
+    {
+        title: "Amazon India Sales Dashboard (Tableau)",
+        link: "https://github.com/anuragpras/amazon-india-sales-dashboard-tableau",
+        description: "Interactive Tableau dashboard analyzing Amazon India's sales data",
+        gif: "3.gif"
+    },
+    {
+        title: "Exploratory Data Analysis",
+        link: "https://github.com/anuragpras/exploratory-data-analysis",
+        description: "E-Commerce Sales Insights: An Exploratory Data Analysis with Python",
+        gif: "4.gif"
+    },
+    {
+        title: "Pizza Sales Analysis",
+        link: "https://github.com/anuragpras/pizza-sales-analysis",
+        description: "Analyzing pizza sales data using MySQL and Excel",
+        gif: "5.gif"
+    },
+    {
+        title: "Not By Gulzar (Random Poetry Generator)",
+        link: "https://github.com/anuragpras/not-by-gulzar",
+        description: "Capture Gulzar's essence with the Random Poetry Generator",
+        gif: "6.gif"
+    },
+    {
+        title: "COVID-19 Dashboard (Tableau)",
+        link: "https://github.com/anuragpras/covid19-tableau",
+        description: "Interactive
